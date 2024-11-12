b602ac689c051d2153ece8ea88c2dfe8f64695e1
Ionita Alin <alin.ionita91@gmail.com>
2024-11-12 15:24:00 +0200

    added jokes library

---
+++ index.js
@@ -0,0 +1,19 @@
+const numbers = [1, 3, 5, 10, 100, 244, 533, 999, 561];
+
+//[{value:1, even: false}, ...... , {value:10, even:true}]
+
+function checkNumbers(nums) {
+    const results = [];
+    
+    for(let i = 0; i < numbers.length; i++) {
+        const current = numbers[i];
+        const isEven = current % 2 === 0;
+        const res = {value: current, even: isEven};
+        console.log(res);
+        results.push(res);
+    }
+
+    console.log(results);
+}
+
+checkNumbers(numbers);
